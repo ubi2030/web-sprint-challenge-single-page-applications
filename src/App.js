a41b1ab799063d7fc63970db2ab100e8bec17475
Jonathan Herzog <jonathan@Jonathans-MacBook-Pro-3.local>
2022-11-13 13:55:38 -0500

    pass all tests

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React, { useState, useEffect } from "react";
 import './App.css';
 import { Route, Link, Switch } from 'react-router-dom';
-import axios from 'axios';
 
 import Home from './components/Home';
 import Pizza from './components/pizza';
-import * as yup from "yup";
 
-
-export default function App(props) {
+export default function App() {
 
 return (
   <div className='App'>
